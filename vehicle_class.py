82fa38d1233f4615a42a2d22128c8545b1822ee7
Chris Owens <guitarrockstarco@gmail.com>
2020-08-03 16:01:46 -0400

    Update vehicle_class.py

--- vehicle_class.py
+++ vehicle_class.py
@@ -6,11 +6,16 @@ class Vehicle:
         self.color = color
 
     def __str__(self):
-        return "Make: %s, Model: %s, Year: %s, Color: %s" % (self.make, self.model, self.year, self.color)
+        return "Make: %s, Model: %s, Year: %d, Color: %s" % (self.make, self.model, self.year, self.color)
+
+    def wheels(self, wheels):
+        return "Wheels: %s" % wheels
 
 
 # Make Model, Year
-
+# Instantiate the class
 honda = Vehicle("Honda", "Civic", 1998, "Silver")
 
 print(honda)
+
+print(honda.wheels(4))
